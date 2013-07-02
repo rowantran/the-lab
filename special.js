d6421e9565fa5a2dd07b4d335267da777365c145
Rowan-James Tran <rj@ThinkPadT61.(none)>
2013-07-02 11:14:45 -0700

    faf

--- special.js
+++ special.js
@@ -12,6 +12,7 @@ var startSim2 = function() {
 		$("#picarea").text("");
 		$("#picarea").append("<img src='metalrust1.png' width=600 height=450>");
 		$("#caption").text("Now here is that same piece of metal, but rusted. Here is an explanation as to how this could have happened: When water droplets gather on an iron object, the water combines with the carbon dioxide and creates a weak acid, which starts to dissolve the iron. Soon some water begins to break down into its chemical components, one of which is oxygen. This oxygen and the dissolved iron turn into iron oxide, also known as rust.");
+		stage = 2;
 	} else if (stage == 2) {
 		$("body").text("");
 		$("head").append("<style>body{background-color: #000000; color: #cc5079;}</style>");
