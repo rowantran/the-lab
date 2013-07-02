d3442dd1abd65f6abead91e1838afe1d50d01fa4
Rowan-James Tran <rj@ThinkPadT61.(none)>
2013-07-02 10:51:55 -0700

    Debugged CV

--- composition.js
+++ composition.js
@@ -1,16 +1,21 @@
 var displayComposition = function(molecule) {
-  $("#currentview").text("Currently viewing:")
+  $("#currentview").text("Currently viewing:");
+  $("#image").text("");
   if (molecule == "salt") {
     var text = " <b>Table salt</b>";
-    this.src = "salt.png";
+    $("#image").append("<img src='salt.png'>");
   } else if (molecule == "water") {
     var text = " <b>Water</b>";
+    $("#image").append("<img src='water.png'>");
   } else if (molecule == "co2") {
     var text = " <b>Carbon dioxide</b>";
+    $("#image").append("<img src='co2.png'>");
   } else if (molecule == "oxygen") {
     var text = " <b>Oxygen</b>";
+    $("#image").append("<img src='oxygen.png'>");
   } else {
     var text = " <b>Deoxyribonucleic acid</b>";
+    $("#image").append("<img src='dna.png'>")
   }
   $("#currentview").append(text);
 };
